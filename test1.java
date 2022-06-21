c6aa25513f17f496d41dab39fcb49b30493882d8
jinji-lab <52945439+jinji-lab@users.noreply.github.com>
2022-06-21 08:42:59 +0800

    Create test1.java
    the number of data type

---
+++ test1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class anvil {
+    //Main method
+    public static void main(String[] args){
+        float f = 12.5f;
+        int i=(int)f;
+        System.out.println(f);
+        System.out.println(i);
+    }
+}
